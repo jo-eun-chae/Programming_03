5eb9aa033117c4d411d9db6ac5326beedeef23e8
jo-eun-chae <67442710+jo-eun-chae@users.noreply.github.com>
2020-12-09 16:25:12 +0900

    Update MyPanel.java

--- MyPanel.java
+++ MyPanel.java
@@ -10,24 +10,15 @@ import java.awt.event.*;
  */
 public class MyPanel extends JPanel implements ActionListener
 {
-    JComboBox<String> cb_grade;
-    JButton b_add;
-    JButton b_clear;
-    JTextArea textArea;
-    
     JTextField tfName;
     JTextField tfDepartment;
     JTextField tfAddress;
+    JComboBox<String> cb_grade;
     
+    JButton b_add;
+    JButton b_clear;
+    JTextArea textArea;
     public MyPanel(){
-        String[] grade = {"1학년", "2학년", "3학년", "4학년"};
-        cb_grade = new JComboBox<String>(grade);
-        
-        b_add = new JButton("추가");
-        b_clear = new JButton("Clear");
-        textArea = new JTextArea(15,25);
-        
-
         JLabel lbName = new JLabel("이름");
         JLabel lbDepartment = new JLabel("학과");
         JLabel lbAddress = new JLabel("주소");
@@ -37,6 +28,13 @@ public class MyPanel extends JPanel implements ActionListener
         tfDepartment = new JTextField("글로벌소프트웨어학과", 20);
         tfAddress = new JTextField("아산시 ...", 20);
         
+        String[] grade = {"1학년", "2학년", "3학년", "4학년"};
+        cb_grade = new JComboBox<String>(grade);
+        
+        b_add = new JButton("추가");
+        b_clear = new JButton("Clear");
+        textArea = new JTextArea(15, 25);
+                
         this.add(lbName);
         this.add(tfName);
         this.add(lbDepartment);
@@ -45,30 +43,19 @@ public class MyPanel extends JPanel implements ActionListener
         this.add(tfAddress);
         this.add(lbGrade);
         this.add(cb_grade);
+        
         this.add(b_add);
         this.add(b_clear);
         this.add(new JScrollPane(textArea));
-        
-        tfName.addActionListener(this);
-        tfDepartment.addActionListener(this);
-        cb_grade.addActionListener(this);
-        tfAddress.addActionListener(this);
-        
-    }
-    
-    public void actionPerformed(ActionEvent e){
-        String text1 = tfName.getText();
-        textArea.append("이름 : " + tfName.getText() + "\n");
-        
-        String text2 = tfDepartment.getText();
-        textArea.append("학과 : " + tfDepartment.getText() + "\n");
-        
-        String text3 = cb_grade.getSelectedItem().toString();
-        textArea.append("학년 : " + text3 + "\n");
-        
-        String text4 = tfAddress.getText();
-        textArea.append("주소 : " + tfAddress.getText() + "\n");
-        textArea.append("---------------------------------------------\n");
 
+        b_add.addActionListener(this);
+        //b_clear.addActionListener(this);
+    }
+    public void actionPerformed(ActionEvent e){
+        textArea.append("이름 : " + tfName.getText());
+        textArea.append("\n학과 : " + tfDepartment.getText());
+        textArea.append("\n학년 : ");
+        textArea.append("\n주소 : " + tfAddress.getText());
+        textArea.append("\n---------------------\n");
     }
 }
