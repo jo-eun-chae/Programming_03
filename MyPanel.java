4001b435ca16dfea8197d3b91fffd6fc925696d3
youchew14 <youchew14@naver.com>
2020-12-09 15:10:01 +0900

    TextArea Size & lable grade add

--- MyPanel.java
+++ MyPanel.java
@@ -21,12 +21,13 @@ public class MyPanel extends JPanel
         
         b_add = new JButton("추가");
         b_clear = new JButton("Clear");
-        textArea = new JTextArea(30,30);
+        textArea = new JTextArea(15,25);
         
 
         JLabel lbName = new JLabel("이름");
         JLabel lbDepartment = new JLabel("학과");
         JLabel lbAddress = new JLabel("주소");
+        JLabel lbGrade = new JLabel("학년");
         
         JTextField tfName = new JTextField(20);
         JTextField tfDepartment = new JTextField("글로벌소프트웨어학과", 20);
@@ -38,6 +39,7 @@ public class MyPanel extends JPanel
         this.add(tfDepartment);
         this.add(lbAddress);
         this.add(tfAddress);
+        this.add(lbGrade);
         this.add(cb_grade);
         this.add(b_add);
         this.add(b_clear);
